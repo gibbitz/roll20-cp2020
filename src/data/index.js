507f9e1ffa7b48af1358ebcb35ea18e1f1c0f24c
Paul Fox <studio@gibbitz.com>
2020-08-12 02:55:34 -0400

    feat: MEGA COMMIT. Added Skills markup and initial styling + tab control =====================================
    ------------------------------------
    [ NA ] pfox@360i

---
+++ src/data/index.js
@@ -0,0 +1,14 @@
+const skills = require('./skills.json')
+const specialAbilities = require('./special-abilities.json')
+const compoundSkills = require('./compound-skills.json')
+const roles = require('./roles.json')
+
+module.exports = {
+  data: {
+    skills,
+    specialAbilities,
+    compoundSkills,
+    roles,
+    test: 'fat-tire'
+  }
+}
