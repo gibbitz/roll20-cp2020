69885d8437bfa14b91d45e9691102ed8fa1b3dab
Paul Fox <studio@gibbitz.com>
2020-08-20 02:55:39 -0400

    chore | fix : Removed comments + fixed CompoundSkills ipMultipliers =====================================
    ------------------------------------
    [ NA ] pfox@360i

--- src/sheet-workers/skills.js
+++ src/sheet-workers/skills.js
@@ -2,7 +2,9 @@
 var stats = data.stats;
 var pickupSkills = data.skills;
 var specialAbilities = data.specialAbilities;
-var skills = pickupSkills.concat(specialAbilities);
+var compoundSkills = data.compoundSkills;
+var skills = pickupSkills.concat(specialAbilities, compoundSkills);
+var potentialCompoundSkills = compoundSkills.concat(specialAbilities);
 var calcSkillEvents = [
   'change:Wound_Level',
   'change:Stun_Level',
@@ -13,6 +15,11 @@ var statKeys = Object.keys(stats) || [];
 statKeys.forEach(function(stat) {
   calcSkillEvents.push(`change:${stat}_Base`);
   calcSkillEvents.push(`change:${stat}_Mod`);
+});
+
+potentialCompoundSkills.forEach(function(skill) {
+  var skillAttrName = `Skill_${window.nameToAttrName(skill.skillName)}`;
+  calcSkillEvents.push(`change:${skillAttrName}_name`);
 });
 
 skills.forEach(function(skill) {
@@ -30,27 +37,33 @@ on(calcSkillEvents, function(eventInfo) {
       skills.forEach(function(skill) {
         if(skill.skillName){
           var attrName = `Skill_${nameToAttrName(skill.skillName)}`;
-          attrsToFetch.push(`${attrName}_level`)
-          attrsToFetch.push(`${attrName}_ip`)
+          attrsToFetch.push(`${attrName}_level`);
+          attrsToFetch.push(`${attrName}_ip`);
+          attrsToFetch.push(`${attrName}_name`)
         }
       });
-
       getAttrs(attrsToFetch, function(skillAttrs) {
         var attrsToSet = {};
-        skills.forEach(function(skill) {
+        skills.forEach(function(skill, index) {
           if(skill.skillName) {
+            var subSkillName = skillAttrs[`Skill_${nameToAttrName(skill.skillName)}_name`]
+            var subSkill = subSkillName ? skills.filter(function(subSkill) {
+              return subSkill.skillName === subSkillName;
+            })[0] : {}
             var skillAttrName = `Skill_${nameToAttrName(skill.skillName)}`;
             var skillLevel = skillAttrs[`${skillAttrName}_level`]
             var skillIp = skillAttrs[`${skillAttrName}_ip`]
             if( skillIp && skillLevel) {
               var level = makeInt(skillLevel);
               var ip = makeInt(skillIp);
-              var ipx = makeInt(skill.ipMultiplier, 1);
+              var ipx = makeInt(
+                (subSkill ? subSkill.ipMultiplier : skill.ipMultiplier),
+                1
+              );
               var stat = makeInt(stats[skill.baseAttribute]);
               stat = isNaN(stat) ? 0 : stat;
               attrsToSet[`${skillAttrName}_next_level`] = ((level + 1) * ipx * 10) - ip;
               attrsToSet[`${skillAttrName}_roll_total`] = level + stat;
-              console.log(`total: ${level}(level) + ${stat}(${skill.baseAttribute})`);
             }
           }
         });
